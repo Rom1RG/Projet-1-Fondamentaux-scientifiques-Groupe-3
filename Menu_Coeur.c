8167acebce2c5342cb8bf8d1a149a51486e0bc6d
Rom1RG <44493181+Rom1RG@users.noreply.github.com>
2018-11-16 12:53:16 +0100

    Update Menu_Coeur.c

--- Menu_Coeur.c
+++ Menu_Coeur.c
@@ -8,11 +8,11 @@ int temps;
 int LED;
 void main ()
 {
-    choix = menu();
+    choix = menu(); //Genere le param.h
     if (choix == 5)
     {
-        printf(" Choississez quelle LED allumer\n");
+        printf(" Choississez quelle LED allumer\n"); //Si le choix est 5 une led s'allume (selon notre choix).
         scanf ("%d", &LED );
     }
-    ecrire_fichier(choix, temps, LED);
+    ecrire_fichier(choix, LED); //Appelle la fonction qui permet de generer le param.h avec les paramètres demandé.
 }
